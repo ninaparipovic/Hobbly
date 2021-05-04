18c254a763efd0a5a4e48cc142f78c3e7db5e25e
ninaparipovic <nina.paripovic.22@dartmouth.edu>
2021-05-03 22:11:44 -0400

    Initial Commit

---
+++ Hobbly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hobbly
+//
+//  Created by Nina Paripovic on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
